f22fa8743860e407e0643abc9a8cd8e59832eebe
[dyad] <git@dyad.sh>
2025-08-07 21:52:48 +0200

    [dyad] Fixed broken Manage Travel page - wrote 2 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,4 @@
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 import Index from "./pages/Index";
 import Blog from "./pages/Blog";
@@ -16,44 +12,28 @@ import ManageBlog from "./pages/ManageBlog";
 import ManageVideos from "./pages/ManageVideos";
 import ManageTravel from "./pages/ManageTravel";
 import Login from "./pages/Login";
-import { TravelProvider } from "./contexts/TravelContext";
-import { AuthProvider } from "./contexts/AuthProvider";
 import ProtectedRoute from "./components/ProtectedRoute";
 
-const queryClient = new QueryClient();
-
 const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <AuthProvider>
-          <TravelProvider>
-            <Routes>
-              <Route path="/login" element={<Login />} />
-              <Route element={<Layout />}>
-                <Route path="/" element={<Index />} />
-                <Route path="/blog" element={<Blog />} />
-                <Route path="/blog/:id" element={<Post />} />
-                <Route path="/videos" element={<Videos />} />
-                <Route path="/gallery" element={<Gallery />} />
-                <Route path="/travel" element={<Travel />} />
-                <Route path="/contact" element={<Contact />} />
-                
-                <Route element={<ProtectedRoute />}>
-                  <Route path="/manage-blog" element={<ManageBlog />} />
-                  <Route path="/manage-videos" element={<ManageVideos />} />
-                  <Route path="/manage-travel" element={<ManageTravel />} />
-                </Route>
-              </Route>
-              <Route path="*" element={<NotFound />} />
-            </Routes>
-          </TravelProvider>
-        </AuthProvider>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
+  <Routes>
+    <Route path="/login" element={<Login />} />
+    <Route element={<Layout />}>
+      <Route path="/" element={<Index />} />
+      <Route path="/blog" element={<Blog />} />
+      <Route path="/blog/:id" element={<Post />} />
+      <Route path="/videos" element={<Videos />} />
+      <Route path="/gallery" element={<Gallery />} />
+      <Route path="/travel" element={<Travel />} />
+      <Route path="/contact" element={<Contact />} />
+      
+      <Route element={<ProtectedRoute />}>
+        <Route path="/manage-blog" element={<ManageBlog />} />
+        <Route path="/manage-videos" element={<ManageVideos />} />
+        <Route path="/manage-travel" element={<ManageTravel />} />
+      </Route>
+    </Route>
+    <Route path="*" element={<NotFound />} />
+  </Routes>
 );
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,32 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./globals.css";
 import { ThemeProvider } from "./components/ThemeProvider.tsx";
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter } from "react-router-dom";
+import { TravelProvider } from "./contexts/TravelContext.tsx";
+import { AuthProvider } from "./contexts/AuthProvider.tsx";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ThemeProvider defaultTheme="light" storageKey="portfolio-theme">
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <TooltipProvider>
+          <Toaster />
+          <Sonner />
+          <BrowserRouter>
+            <AuthProvider>
+              <TravelProvider>
+                <App />
+              </TravelProvider>
+            </AuthProvider>
+          </BrowserRouter>
+        </TooltipProvider>
+      </QueryClientProvider>
     </ThemeProvider>
   </React.StrictMode>,
 );
