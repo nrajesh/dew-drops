bbc8875855a34246ecf78f0781f075d7f9c6099f
[dyad] <Narayanan.Rajesh@outlook.com>
2025-08-10 13:16:21 +0200

    [dyad] Added Vercel Speed Insights - wrote 3 file(s), added @vercel/speed-insights package(s)

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import { ThemeProvider } from "./components/ThemeProvider.tsx";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter } from "react-router-dom";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 const queryClient = new QueryClient();
 
@@ -16,6 +17,7 @@ createRoot(document.getElementById("root")!).render(
         <QueryClientProvider client={queryClient}>
           <TooltipProvider>
             <App />
+            <SpeedInsights />
           </TooltipProvider>
         </QueryClientProvider>
       </ThemeProvider>
