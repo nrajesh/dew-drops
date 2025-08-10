4206afe19cbc387fb4200689551882312aeec806
[dyad] <Narayanan.Rajesh@outlook.com>
2025-08-10 12:05:19 +0200

    [dyad] Fixed syntax error in travel management page - wrote 1 file(s)

--- src/pages/ManageTravel.tsx
+++ src/pages/ManageTravel.tsx
@@ -257,7 +257,7 @@ const ManageTravel = () => {
         <CardHeader>
           <CardTitle>Travel Log</CardTitle>
           <CardDescription>Your current list of visited places.</CardDescription>
-        </Header>
+        </CardHeader>
         <CardContent>
           <div className="space-y-4">
             {locations.length > 0 ? (
