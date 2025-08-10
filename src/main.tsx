7cd6238b16ab043e8d90bb08400dd78fb315fc64
[dyad] <Narayanan.Rajesh@outlook.com>
2025-08-10 13:17:52 +0200

    Updated src/main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { ThemeProvider } from "./components/ThemeProvider.tsx";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter } from "react-router-dom";
-import { SpeedInsights } from "@vercel/speed-insights/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const queryClient = new QueryClient();
 
