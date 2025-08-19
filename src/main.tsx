fa58b24d84dd9609bd1b0a20ef3c9cecfce82908
[dyad] <Narayanan.Rajesh@outlook.com>
2025-08-19 17:09:49 +0200

    Updated src/main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -14,7 +14,7 @@ const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter future={{ v7_startTransition: true }}>
       <ThemeProvider
         attribute="class"
         defaultTheme="dark"
