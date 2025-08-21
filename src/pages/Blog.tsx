149c81dccf816a1099b55a4fea62e7c0773f64e6
[dyad] <Narayanan.Rajesh@outlook.com>
2025-08-21 21:48:46 +0200

    Reverted all changes back to version 6a150323fdeca2e9a78cb9b5d7dfd0887be6dd94

--- src/pages/Blog.tsx
+++ src/pages/Blog.tsx
@@ -167,7 +167,7 @@ const Blog = () => {
           )}
         </div>
       </div>
-      <div className="mt-4">
+      <div className="mt-6">
         <PaginationControls
           currentPage={currentPage}
           totalPages={totalPages}
--- src/pages/Gallery.tsx
+++ src/pages/Gallery.tsx
@@ -165,7 +165,7 @@ const Gallery = () => {
             )}
           </div>
         </div>
-        <div className="mt-4">
+        <div className="mt-6">
           <PaginationControls currentPage={currentPage} totalPages={totalPages} onPageChange={setCurrentPage} />
         </div>
       </div>
